09d023ce7eefc0d55d49b1e3be01adffd1c773a1
Leandro <vitto.leandro@gmail.com>
2021-05-12 00:09:29 +0200

    Node e Pusher

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from "./model/dbMessages.js";
 
+import Pusher from 'pusher'
+
+let pusher = new Pusher({
+    appId: "1202355",
+    key: "139e3b7703cbe6a66c04",
+    secret: "1b30aeaf7541f4d0534d",
+    cluster: "eu",
+    useTLS: true
+});
+
 const app = express();
-
-//new-user
-// vjWCiR4wnET7r5hw
 const port = process.env.NODE_PORT || 9000;
-
 app.use(express.json());
 //app.use(cors());
-
 const connectionUrl = "mongodb+srv://new-user:vjWCiR4wnET7r5hw@cluster0.yqgw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+                      // mongodb+srv://new-user:vjWCiR4wnET7r5hw@cluster0.yqgw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 mongoose.connect(
     connectionUrl, 
     {
@@ -28,6 +33,28 @@ mongoose.connect(
         }
     }
 );
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Db Connected")
+
+    const msgCollection = db.collection("messagecontents")
+    const changeStream = msgCollection.watch();
+
+    changeStream.on('change',(change) => {
+        console.log(change)
+        if(change.operationType === "insert"){
+            const record = change.fullDocument;
+            pusher.trigger("messages", "inserted", {
+                name : record.name,
+                message : record.message
+            })
+        }else{
+            console.log("Not Trigger Pusher")
+        }
+    })
+})
 
 app.get("/api/v1/messages/sync", (req,res) => {
     Messages.find((err,data) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './sidebar/Sidebar';
 import Chat from './chat/Chat';
+import { useEffect, useState } from 'react';
+import Pusher from 'pusher-js'
 
 function App() {
+  const [message,setMessage] = useState("MESSAGGIO");
+
+  useEffect(() => {
+    const pusher = new Pusher('139e3b7703cbe6a66c04', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe(`messages`);
+
+    channel.bind('inserted', function(data) {
+      //alert(JSON.stringify(data));
+      setMessage(data.message)
+    });
+ 
+  }, [])
+
+
   return (
     <div className="app">
+      <h1>{message}</h1>
       <div className="app__body">
         <Sidebar />
         <Chat />
